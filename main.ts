a82693b441c189248007a10c7c23d44b5eed8805
KBergmanis <91208092+KBergmanis@users.noreply.github.com>
2021-11-06 20:55:08 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,16 +1,17 @@
-led.enable(false)
-basic.forever(function () {
-    pins.digitalWritePin(DigitalPin.P0, 1)
+input.onPinPressed(TouchPin.P1, function () {
+    pins.digitalWritePin(DigitalPin.P7, 1)
     basic.pause(5000)
-    pins.digitalWritePin(DigitalPin.P0, 0)
+    pins.digitalWritePin(DigitalPin.P7, 0)
     for (let index = 0; index < 3; index++) {
         basic.pause(500)
-        pins.digitalWritePin(DigitalPin.P1, 1)
+        pins.digitalWritePin(DigitalPin.P8, 1)
         basic.pause(500)
-        pins.digitalWritePin(DigitalPin.P1, 0)
+        pins.digitalWritePin(DigitalPin.P8, 0)
     }
     basic.pause(500)
-    pins.digitalWritePin(DigitalPin.P2, 1)
+    pins.digitalWritePin(DigitalPin.P9, 1)
     basic.pause(500)
-    pins.digitalWritePin(DigitalPin.P1, 0)
+    pins.digitalWritePin(DigitalPin.P9, 0)
+    basic.pause(500)
 })
+led.enable(false)
